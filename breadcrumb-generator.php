d12608674a8e00fb8747b13af3a9b0a772a972cd
Ryan Short <ryanshort.dev@gmail.com>
2025-11-03 09:22:55 -0500

    made a comment more specific

--- breadcrumb-generator.php
+++ breadcrumb-generator.php
@@ -1,6 +1,6 @@
 <?php
 // use [iiq_breadcrumbs] shortcode to add breadcrumbs to custom post type templates
-// or change line 43 to use different shortcode, if needed for a different project
+// or change line 43 to use different, non-specific shortcode
 
 if ( ! function_exists( 'iiq_get_primary_meta_cat_id' ) ) {
 	function iiq_get_primary_meta_cat_id( $post_id = 0 ){
